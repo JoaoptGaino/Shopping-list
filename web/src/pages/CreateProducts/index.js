91929c069924d140a57a651df02a3553e7230d3b
joaoptgaino <joaoptgaino@hotmail.com>
2020-06-28 23:47:05 -0300

    Commit some bugs so I can fix it later I need a fucking job please

--- web/src/pages/CreateProducts/index.js
+++ web/src/pages/CreateProducts/index.js
@@ -1,9 +1,51 @@
-import React from 'react';
-
+import React,{useState,useEffect,FormEvent,ChangeEvent} from 'react';
+import {useHistory,Link} from 'react-router-dom';
+import api from '../../services/api';
 
 const CreateProduct = () =>{
+    const [products,setProducts] = useState();
+    const [formData,setFormData] = useState({
+        name:''
+    });
+    const history = useHistory();
+    function handleInputChange(input){
+        input.preventDefault();
+        const {name,value} = input.target;
+        /* console.log(name); */
+        setFormData({...formData,[name]:value});
+        console.log(formData);
+    }
+    async function handleSubmit(event){
+        /* event = new FormEvent(); */
+        event.preventDefault();
+        //console.log(formData);
+        const name = formData.name;
+        /* const nameReal = JSON.stringify({name});
+        console.log(nameReal); */
+        const data = new FormData();
+
+        data.append('name',name);
+        
+        await api.post('api/products',data.get("name"));
+        
+    }
+
     return(
-        <h1>Oi</h1>
+        <div className="content">
+            <form onSubmit={handleSubmit}>
+                <div className="card">
+                    <div className="card-body">
+                        <div className="form-group">
+                            <label htmlFor="name">Nome do produto</label>
+                            <input type="text" className="form-control" id="name" name="name" onChange={handleInputChange}/>
+                        </div>
+                    </div>
+                    <div className="card-footer">
+                        <button type="submit" className="btn btn-primary">Cadastrar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
--- web/src/pages/Products/index.js
+++ web/src/pages/Products/index.js
@@ -1,31 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
 import './styles.css';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     api.get('/api/products')
-    .then(response => {
+      .then(response => {
         setProducts(response.data);
-    })
-  },[{}]);
+      })
+  }, [{}]);
   return (
 
     <div className="content">
-    <h2 class="title">Shopping list</h2>
-    <div className="row">
-        <div className="col-md-6" id="toBuy">
-            Comprar
+      <h2 className="title">Shopping list</h2>
+      <div className="row">
+        <div className="col-md-4" id="toBuy">
+          <h1>Comprar</h1>
         </div>
-        <div className="col-md-6" id="products">
-        {
-        products.map(product=>(
-          <li draggable>{product.name}</li>
-        ))
-      }
+        <div className="col-md-4" id="buttons">
+          <h1>Botoes</h1>
+          <ul>
+            {/* <li><button className="addList">Add to list</button></li> */}
+            <li><button className="removeLatest">Remove latest</button></li>
+            <li><button className="removeAll">Remove all</button></li>
+          </ul>
+
         </div>
-    </div>
+        <div className="col-md-4" id="products">
+          <h2>Products</h2>
+          {
+            products.map(product => (
+              <li key={product.id}><FiArrowLeft />{product.name}</li>
+            ))
+          }
+          <Link to="/create-product" className="linkButton">Add new product</Link>
+        </div>
+      </div>
     </div>
   );
 }
